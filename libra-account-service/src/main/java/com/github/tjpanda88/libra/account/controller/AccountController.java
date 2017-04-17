6d15f09dea13d25fd24e78a5e3ecc29d7d708a41
天魂无双 <tjpanda88@sina.com>
2017-04-17 22:25:04 +0800

    add:添加mysql引用

--- libra-account-service/src/main/java/com/github/tjpanda88/libra/account/controller/AccountController.java
+++ libra-account-service/src/main/java/com/github/tjpanda88/libra/account/controller/AccountController.java
@@ -3,16 +3,35 @@ package com.github.tjpanda88.libra.account.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.tjpanda88.libra.account.domain.Account;
+import com.github.tjpanda88.libra.account.repository.AccountRepository;
 import com.github.tjpanda88.libra.account.resource.AccountResource;
 
 @RestController
 public class AccountController implements AccountResource {
 
-	public Map<String,String> getCurrentAccount() {
-		Map<String,String> result = new HashMap<String,String>();
+	@Autowired
+	private AccountRepository accountRepository;
+
+	public Map<String, String> getCurrentAccount() {
+
+		Account temp = accountRepository.findByName("My");
+
+		if (temp == null) {
+			Account entity = new Account();
+			entity.setAge(15);
+			entity.setName("My");
+			accountRepository.save(entity);
+		}
+		
+		Account searchEntity = accountRepository.findByName("My");
+
+		Map<String, String> result = new HashMap<String, String>();
 		result.put("test", "test");
+		result.put("age", searchEntity.getAge().toString());
 		return result;
 	}
 
